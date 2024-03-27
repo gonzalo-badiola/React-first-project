52bb89bb11ac6c51d1436a5cac04c60d135980c4
Gonzalo Badiola <gonzalo.badiola@globant.com>
2024-03-27 10:28:52 -0300

    "First Commit"

---
+++ src/bases/01_const_let.js
@@ -0,0 +1,18 @@
+// variables
+
+const nombre = "Fernando";
+const apellido = "Herrera";
+
+let valorDado = 5; 
+valorDado = 4;
+
+console.log( nombre, apellido, valorDado );
+
+// var No se debe de usar
+if (true) { 
+    const nombre = "Peter"
+    // let valorDado = 6;
+    console.log(nombre)
+}
+
+console.log(valorDado); 
