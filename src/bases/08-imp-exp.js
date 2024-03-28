991dc354737af524336a774c2543f2b7f1e225bc
Gonzalo Badiola <gonzalo.badiola@globant.com>
2024-03-28 17:55:27 -0300

    "fetch"

--- src/bases/08-imp-exp.js
+++ src/bases/08-imp-exp.js
@@ -1,20 +1,23 @@
-import heroes, {owners} from "./data/heroes";
+import heroes from "../data/heroes"
 
 
-console.log(owners)
+
+
+// console.log(owners)
 // const getHeroeById = (id) => {
 //     return heroes.find((heroe) => heroe.id === id)
 // }
 
 
 // Hice yo funciona
-const getHeroeById = (id) => heroes.find((heroe) => heroe.id === id)
+export const getHeroeById = (id) => heroes.find((heroe) => heroe.id === id)
 
 
 
- console.log( getHeroeById(2) );
+//  console.log( getHeroeById(2) );
 
 
- const getHeroesbyOwner = (owner) => heroes.filter((heroe) => heroe.owner === owner)
+ export const getHeroesbyOwner = (owner) => heroes.filter((heroe) => heroe.owner === owner)
 
- console.log(getHeroesbyOwner("DC"))
+//  console.log(getHeroesbyOwner("DC"))
+
